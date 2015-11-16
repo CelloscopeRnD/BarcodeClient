bd4d49c250e335ef2343ba5751e08e3a2a63b4be
zamanAEL <assad.zaman@gmail.com>
2015-11-16 22:29:55 +0600

    add feature

--- app/src/main/java/co/celloscope/barcodeclient/MainActivity.java
+++ app/src/main/java/co/celloscope/barcodeclient/MainActivity.java
@@ -1,15 +1,12 @@
 package co.celloscope.barcodeclient;
 
+import android.app.AlertDialog;
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     public static final String BARCODE_READER_CLASS_NAME = "co.celloscope.barcodereader.MainActivity";
     public static final String BARCODE_READER_PACKEAGE_NAME = "co.celloscope.barcodereader";
     public static final int REQUEST_CODE_READ_BARCODE = 1;
+    public static final String NAME = "NAME";
+    private static final String PIN = "PIN";
+    private static final String DOB = "DOB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +35,30 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE_READ_BARCODE && resultCode == RESULT_OK) {
-            Toast.makeText(MainActivity.this, "result", Toast.LENGTH_SHORT).show();
+        if (requestCode == REQUEST_CODE_READ_BARCODE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(MainActivity.this, new StringBuilder(data.getStringExtra(NAME))
+                        .append(data.getStringExtra(PIN))
+                        .append(data.getStringExtra(DOB))
+                        .toString(), Toast.LENGTH_LONG).show();
+                new AlertDialog.Builder(this)
+                        .setMessage(new StringBuilder("Name: ")
+                                .append(data.getStringExtra(NAME))
+                                .append("\nPin: ")
+                                .append(data.getStringExtra(PIN))
+                                .append("\nDOB: ")
+                                .append(data.getStringExtra(DOB))
+                                .toString())
+                        .setCancelable(false)
+                        .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).show();
+            } else {
+                Toast.makeText(MainActivity.this, R.string.noResultFound, Toast.LENGTH_LONG).show();
+            }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
