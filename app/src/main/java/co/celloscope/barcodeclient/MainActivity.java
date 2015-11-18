17181fa8a70849d9678c415e3adde5c8548106cb
zamanAEL <assad.zaman@gmail.com>
2015-11-18 13:28:27 +0600

    Refactoring

--- app/src/main/java/co/celloscope/barcodeclient/MainActivity.java
+++ app/src/main/java/co/celloscope/barcodeclient/MainActivity.java
@@ -18,9 +18,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String PIN = "PIN";
     private static final String DOB = "DOB";
     private static final String BARCODE_TYPE = "BarcodeType";
-    private static final String PDF417 = "PDF417";
     private static final String BARCODE_CONTENT = "BarcodeContent";
+    private static final String QR = "QR";
+    private static final String PDF417 = "PDF417";
+    private static final String ABC = "ABC";
     private static final String NID = "NID";
+    private boolean isPDF417RequestSend = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,27 +33,40 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.readBarcodeButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.putExtra(BARCODE_TYPE, PDF417);
-                intent.putExtra(BARCODE_CONTENT, NID);
-                intent.setComponent(new ComponentName(BARCODE_READER_PACKAGE_NAME, BARCODE_READER_CLASS_NAME));
-                startActivityForResult(intent, REQUEST_CODE_READ_BARCODE);
+                startBarcodeReader(PDF417, NID);
+                isPDF417RequestSend = true;
             }
         });
+
+        findViewById(R.id.readQRCodeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startBarcodeReader(QR, ABC);
+                isPDF417RequestSend = false;
+            }
+        });
+    }
+
+    private void startBarcodeReader(String barcodeType, String barcodeContent) {
+        Intent intent = new Intent();
+        intent.putExtra(BARCODE_TYPE, barcodeType);
+        intent.putExtra(BARCODE_CONTENT, barcodeContent);
+        intent.setComponent(new ComponentName(BARCODE_READER_PACKAGE_NAME, BARCODE_READER_CLASS_NAME));
+        startActivityForResult(intent, REQUEST_CODE_READ_BARCODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE_READ_BARCODE) {
             if (resultCode == RESULT_OK) {
+                String message;
+                if (isPDF417RequestSend) {
+                    message = "Name: " + data.getStringExtra(NAME) + "\nPin: " + data.getStringExtra(PIN) + "\nDOB: " + data.getStringExtra(DOB);
+                } else {
+                    message = "Pin: " + data.getStringExtra(PIN);
+                }
                 new AlertDialog.Builder(this)
-                        .setMessage(new StringBuilder("Name: ")
-                                .append(data.getStringExtra(NAME))
-                                .append("\nPin: ")
-                                .append(data.getStringExtra(PIN))
-                                .append("\nDOB: ")
-                                .append(data.getStringExtra(DOB))
-                                .toString())
+                        .setMessage(message)
                         .setCancelable(false)
                         .setNeutralButton("OK", new DialogInterface.OnClickListener() {
                             @Override
