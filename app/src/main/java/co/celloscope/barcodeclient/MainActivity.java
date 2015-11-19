2ab1fe42350da5cefc66a82d454902e5dfbb6c0b
zamanAEL <assad.zaman@gmail.com>
2015-11-19 20:49:38 +0600

    Inspect code

--- app/src/main/java/co/celloscope/barcodeclient/MainActivity.java
+++ app/src/main/java/co/celloscope/barcodeclient/MainActivity.java
@@ -2,7 +2,6 @@ package co.celloscope.barcodeclient;
 
 import android.app.AlertDialog;
 import android.content.ComponentName;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +10,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String BARCODE_READER_CLASS_NAME = "co.celloscope.barcodereader.MainActivity";
-    public static final String BARCODE_READER_PACKAGE_NAME = "co.celloscope.barcodereader";
-    public static final int REQUEST_CODE_READ_BARCODE = 1;
-    public static final String NAME = "NAME";
+    private static final String BARCODE_READER_CLASS_NAME = "co.celloscope.barcodereader.MainActivity";
+    private static final String BARCODE_READER_PACKAGE_NAME = "co.celloscope.barcodereader";
+    private static final int REQUEST_CODE_READ_BARCODE = 1;
+    private static final String NAME = "NAME";
     private static final String PIN = "PIN";
     private static final String DOB = "DOB";
     private static final String BARCODE_TYPE = "BarcodeType";
